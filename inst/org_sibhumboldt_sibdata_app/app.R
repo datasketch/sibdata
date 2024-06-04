681a3e99527f9c207e3d4cc12a5bfd2042d69efe
jpmarindiaz <jpmarindiaz@gmail.com>
2024-06-04 15:38:28 +0100

    app fixes

--- inst/org_sibhumboldt_sibdata_app/app.R
+++ inst/org_sibhumboldt_sibdata_app/app.R
@@ -7,39 +7,67 @@ library(hgmagic)
 # library(lfltmagic)
 library(shinyinvoer)
 library(dsmods)
-
+library(geotable)
 library(sibdata)
+library(data.tree)
 
-dbdir <- sys_file_sibdata("db/sibdata.sqlite")
-dbdir <- "db/sibdata.sqlite"
-con <- DBI::dbConnect(RSQLite::SQLite(), dbdir,
-                      read_only = TRUE)
+# dbdir <- sys_file_sibdata("db/sibdata.sqlite")
+#dbdir <- sys_file_sibdata("db/sibdata.duckdb")
+dbdir <- "db/sibdata.duckdb"
+con <- gt_con(con = dbdir)
+#duckdbits::duckdb_disconnect()
+# dbdir <- "db/sibdata.sqlite"
+# con <- DBI::dbConnect(RSQLite::SQLite(), dbdir,
+#                       read_only = TRUE)
 
-av_grupos_bio <- sib_available_grupos(tipo = "biologico", con = con)
-opts_grupo_biologico <- c("Todos" = "todos", av_grupos_bio)
+# av_grupos_bio <- sib_available_grupos(tipo = "biologico", con = con)
+# opts_grupo_biologico <- c("Todos" = "todos", av_grupos_bio)
+
 av_grupos_int <- sib_available_grupos(tipo = "interes", con = con)
 opts_grupo_interes <-  c("Todos" = "todos", av_grupos_int)
 
+#
+gru <- sibdata_grupo(con) |> collect() |> filter(tipo == "biologico")
+gru_tree <- data.tree::FromDataFrameNetwork(gru)
+gru_df <- data.tree::ToDataFrameNetwork(gru_tree,
+                                        direction = "descend",
+                                        "label", "level", "path")
+paste_dash <- function(str, times = 1){
+  paste(" ", paste0(rep("-",times-1), collapse = ""),str)
+}
+opt_gru <- gru_df |>
+  rowwise() |>
+  mutate(label = paste_dash(label, level)) |>
+  arrange(path)
+opts_grupo_biologico <- opt_gru$from
+names(opts_grupo_biologico) <- opt_gru$label
+opts_grupo_biologico <- c("Todos" = "todos", opts_grupo_biologico)
+
+
 pais <- sib_available_regions(subtipo = "País", con = con)
 departamentos <- sib_available_regions(subtipo = "Departamento", con = con)
-opts_region <- c(pais, departamentos)
+opts_region <- c(pais, sort(departamentos))
 # opts_region <- c("colombia", "narino", "boyaca", "santander", "tolima",
 #                  "resguardo-indigena-pialapi-pueblo-viejo",
 #                  "reserva-natural-la-planada")
 
-opts_tematicas <- c("Todas" = "todas", sib_available_tematicas())
 
+opts_tematicas <- c("Todas" = "todas", sib_available_tematicas())
+opts_tematicas_ex <- c("cites_i", "cites_ii","cites_i_ii", "cites_iii",
+                       "exoticas", "invasoras", "riesgo_invasion")
+opts_tematicas <- opts_tematicas[!opts_tematicas %in% opts_tematicas_ex]
+opts_tematicas <- c(opts_tematicas, c("Exóticas Total" = "exoticas-total"))
+# opts_tematicas <- gsub("_","-",opts_tematicas) # hay diferencia entre sibdata y list_species
+# uno recibe _ y el otro -
 
 ui <- panelsPage(
   tags$head(
     tags$link(rel="stylesheet", type="text/css", href="custom.css")
   ),
-  panel(title = "Opciones", width = 250,
+  panel(title = "Opciones", width = 280,
         body = div(
-          # verbatimTextOutput("debug"),
+          verbatimTextOutput("debug"),
           uiOutput("sel_region_"),
-          hr(),
-          radioButtons("sel_tipo", "Tipo", c("Observaciones" = "registros","Especies"="especies")),
           hr(),
           uiOutput("sel_grupo_"),
           uiOutput("sel_grupo_opts"),
@@ -60,17 +88,18 @@ ui <- panelsPage(
                                uiOutput("descargas"))
         ),
         body = div(
+          radioButtons("sel_tipo", "Tipo", c("Observaciones" = "registros","Especies"="especies")),
+          hr(),
           uiOutput("controls"),
           uiOutput("chart_controls"),
-
           uiOutput("viz"),
           br()
         ),
         footer = ""),
   panel(title = "Especies",
-        width = 450,
+        width = 400,
         can_collapse = FALSE,
-        # header_right = downloadTableUI("species_table", dropdownLabel = "Descargar especies", formats = c("csv", "xlsx", "json"), display = "dropdown", dropdownWidth = 200),
+        # header_right = downloadTableButtonUI("species_table", dropdownLabel = "Descargar especies", formats = c("csv", "xlsx", "json"), display = "dropdown", dropdownWidth = 200),
         body = dataTableOutput("list_species")
   )
 )
@@ -96,7 +125,10 @@ server <-  function(input, output, session) {
     #url_par()
     #str(r$toList)
     str(inputs())
-    str(data())
+    # str(data())
+    str(data_especies())
+    str(names(opts_grupo_biologico))
+    str(length(opts_grupo_biologico))
   })
 
 
@@ -105,7 +137,7 @@ server <-  function(input, output, session) {
     default_select <- NULL
     if (!is.null(url_par()$region)) default_select <- tolower(url_par()$region)
     selectizeInput("sel_region","Seleccione Región",
-                   rev(opts_region),
+                   opts_region,
                    selected = default_select
     )
   })
@@ -125,15 +157,20 @@ server <-  function(input, output, session) {
 
     opts <- opts_grupo_interes
     id <- "sel_grupo_int"
+    out <- selectInput(id ,
+                       "Seleccione grupo de interés",
+                       opts_grupo_biologico,
+                       default_select)
+
     if (input$sel_grupo_type == "biologico") {
-      opts <- opts_grupo_biologico
       id <- "sel_grupo_bio"
+      out <- selectInput(id ,
+                  "Seleccione grupo biológico",
+                  opts_grupo_biologico,
+                  default_select)
     }
 
-    selectizeInput(id ,
-                   "Seleccione grupo",
-                   opts,
-                   default_select)
+    out
 
   })
 
@@ -150,6 +187,9 @@ server <-  function(input, output, session) {
 
     req(input$sel_grupo_type)
     req(input$sel_tematica)
+    req(input$sel_grupo_bio)
+    # req(input$sel_grupo_int)
+
     subregiones = input$sugrebiones %||% FALSE
     with_parent = input$with_parent %||% FALSE
 
@@ -178,14 +218,30 @@ server <-  function(input, output, session) {
     if (input$sel_grupo_type == "interes") grupo <- input$sel_grupo_int
     req(grupo)
     if (grupo == "todos") grupo <- NULL
-    tematica <- input$sel_tematica
+    tematica <- gsub("_", "-", input$sel_tematica) ## OJO quitar cuando se estandarice _ y - en amenazadas_nacional
     if (tematica == "todas") tematica <- NULL
     l_s <- list_species(region = input$sel_region,
                         grupo = grupo,
                         tematica = tematica,
                         #with_labels = TRUE
                         con = con) |>
-      collect()
+      collect() |>
+      mutate(
+      ) |>
+      select(-species, -flagTAXO, -vernacular_name_es) |>
+      select(-any_of(c("slug_especie", "slug_tematica"))) |>
+      rename(
+        "Especie" = "label",
+        "Registros" = "registros",
+        "Reino" = "kingdom",
+        "GBIF" = "url_gbif",
+        "CBC" = "url_cbc",
+        "Filo" = "phylum",
+        "Clase" = "class",
+        "Orden" = "order",
+        "Familia" = "family",
+        "Género" = "genus"
+      )
     l_s
   })
 
@@ -221,17 +277,18 @@ server <-  function(input, output, session) {
   })
 
   data <- function(){
-      inp <- inputs()
-      subR <- inp$subregiones
-      d <- sibdata(inp$region,
-                   grupo = inp$grupo,
-                   tipo = inp$tipo,
-                   cobertura = inp$cobertura,
-                   tematica = inp$tematica,
-                   subregiones = subR,
-                   with_parent = inp$with_parent,
-                   con = con)
-      d <- d |> sib_merge_ind_label(con = con)
+    req(inputs())
+    inp <- inputs()
+    subR <- inp$subregiones
+    d <- sibdata(inp$region,
+                 grupo = inp$grupo,
+                 tipo = inp$tipo,
+                 cobertura = inp$cobertura,
+                 tematica = inp$tematica,
+                 subregiones = subR,
+                 with_parent = inp$with_parent,
+                 con = con)
+    d <- d |> sib_merge_ind_label(con = con)
     d
 
     # tryCatch({
@@ -274,14 +331,19 @@ server <-  function(input, output, session) {
   })
 
   available_charts <- reactive({
-    #dd <- data()
+    req(data())
+    if(is.null(data())) return(c("Tabla" = "table"))
+    dd <- data()
+    if(nrow(dd) == 1){
+      return(c("Tabla" = "table"))
+    }
     c( "Mapa" = "map", "Torta"= "pie", "Dona" = "donut", "Treemap" = "treemap","Barras" = "bar", "Tabla"="table")
     c("Torta"= "pie", "Dona" = "donut", "Treemap" = "treemap","Barras" = "bar", "Tabla"="table")
   })
 
 
   hover_viz <- reactive({
-    req(available_charts())
+    # req(available_charts())
     names(available_charts())
   })
 
@@ -289,7 +351,8 @@ server <-  function(input, output, session) {
   actual_but <- reactiveValues(active = NULL)
 
   observe({
-    if (is.null(available_charts())) return()
+    # req(available_charts())
+    # if (is.null(available_charts())) return()
     viz_rec <- available_charts() |> as.vector()
     if (is.null(input$sel_chart_type)) return()
     vizDef <- input$sel_chart_type
@@ -421,14 +484,13 @@ server <-  function(input, output, session) {
     if (actual_but$active != "table") {
       # downloadImageUI("download_viz", dropdownLabel = "Descargar", formats = c("jpeg", "pdf", "png", "html"), display = "dropdown")
     } else {
-      # downloadTableUI("dropdown_table", dropdownLabel = "Descargar", formats = c("csv", "xlsx", "json"), display = "dropdown")
+      #downloadTableUI("dropdown_table", dropdownLabel = "Descargar", formats = c("csv", "xlsx", "json"), display = "dropdown")
     }
   })
 
   # downloadTableServer("dropdown_table", element = reactive(data_fin()), formats = c("csv", "xlsx", "json"))
   # downloadImageServer("download_viz", element = reactive(l_viz()), lib = "highcharter", formats = c("jpeg", "pdf", "png", "html"), file_prefix = "plot")
-  # downloadTableServer("species_table", element = reactive(data_especies()), formats = c("csv", "xlsx", "json"))
-
+  # downloadTableButtonServer("species_table", element = reactive(data_especies()), formats = c("csv", "xlsx", "json"))
 
 
 }
--- inst/org_sibhumboldt_sibdata_app/dev.R
+++ inst/org_sibhumboldt_sibdata_app/dev.R
@@ -11,9 +11,11 @@ library(dsmods)
 library(dbplyr)
 
 
-con <- DBI::dbConnect(RSQLite::SQLite(), sys_file_sibdata("db/sibdata.sqlite"),
-                      read_only = TRUE)
+#con <- DBI::dbConnect(RSQLite::SQLite(), sys_file_sibdata("db/sibdata.sqlite"),
+#                      read_only = TRUE)
 
+dbdir <- sys_file_sibdata("db/sibdata.duckdb")
+con <- gt_con(con = dbdir)
 
 av_grupos_bio <- sib_available_grupos(tipo = "biologico", con = con)
 opts_grupo_biologico <- c("Todos" = "todos", av_grupos_bio)
@@ -72,11 +74,30 @@ d <- sibdata(inp$region,
              con = con)
 d
 
+##
+
+input <- list(
+  region = "boyaca",
+  grupo = "animales",
+  tematica = "exoticas-total",
+  subregiones = FALSE,
+  with_parent = FALSE
+)
+region <- inp$region
+d <- sibdata(inp$region,
+             grupo = inp$grupo,
+             tipo = inp$tipo,
+             cobertura = inp$cobertura,
+             tematica = inp$tematica,
+             subregiones = inp$subregiones,
+             with_parent = inp$with_parent,
+             con = con)
+d
+esp
 
 
 
-
-
+##
 esp <- list_species(region = input$region,
              grupo = input$grupo,
              tematica = input$tematica,
@@ -101,9 +122,39 @@ esp
 
 
 
+###
+
+#install.packages("collapsibleTree")
+library(collapsibleTree)
+library(tidyverse)
 
 
+library(data.tree)
 
+gru <- sibdata_grupo(con) |> collect() |> filter(tipo == "biologico") |>
+  arrange()
 
+gru_tree <- data.tree::FromDataFrameNetwork(gru)
+gru_df <- data.tree::ToDataFrameNetwork(gru_tree,
+                                        direction = "descend",
+                                        "label", "level", "path")
+paste_dash <- function(str, times = 1){
+  paste(" ", paste0(rep("-",times-1), collapse = ""),str)
+}
+opt_gru <- gru_df |>
+  rowwise() |>
+  mutate(label = paste_dash(label, level)) |>
+  arrange(path)
+opts_grupo_biologico <- opt_gru$from
+names(opts_grupo_biologico) <- opt_gru$label
+opts_grupo_biologico <- c("Todos" = "todos", opts_grupo_biologico)
+str(names(opts_grupo_biologico))
 
+collapsibleTree(gru_tree,
+                attribute = "label",
+                tooltipHTML = "label",
+                collapsed = TRUE)
+
+tem <- sibdata_tematica(con) |> collect()
+collapsibleTree(tem, c("parent", "slug"))
 
